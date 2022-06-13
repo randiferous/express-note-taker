a2f4a8837574c208959f5836a804b971db79f0a9
lotus <daniel.younghwan.lee@gmail.com>
2022-06-13 17:13:13 -0400

    heroku deployment should work

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,12 +1,12 @@
 const path = require('path');
 const router = require('express').Router();
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
   });
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 const app = express();
 
 const htmlRoutes = require('./routes/htmlRoutes')
