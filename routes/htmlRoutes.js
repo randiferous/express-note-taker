ed8af233b72b748a30f25450a85760e7cb8204b2
lotus <daniel.younghwan.lee@gmail.com>
2022-06-13 16:56:45 -0400

    still figuring out heroku deployment

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const router = require('express').Router();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const express = require('express')
 const PORT = process.env.PORT
 const app = express();
 
+const htmlRoutes = require('./routes/htmlRoutes')
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use('/', htmlRoutes);
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 })
